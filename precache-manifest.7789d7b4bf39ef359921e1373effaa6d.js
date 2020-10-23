b0ed604a34fbab771e73c36e48051f588e8109ce
Tharusha Kudagala <tharushakudagala1997@gmail.com>
2020-10-23 07:40:09 +0530

    Updates

---
+++ precache-manifest.7789d7b4bf39ef359921e1373effaa6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e9b24dd8387b66494eaf0cb10ae8490",
+    "url": "/SL-Covid/index.html"
+  },
+  {
+    "revision": "e7b24c90a2c53fd36f30",
+    "url": "/SL-Covid/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7a6b9be3c59311f34cd7",
+    "url": "/SL-Covid/static/js/2.0af65886.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/SL-Covid/static/js/2.0af65886.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7b24c90a2c53fd36f30",
+    "url": "/SL-Covid/static/js/main.5ab9da3d.chunk.js"
+  },
+  {
+    "revision": "fb49edb0a01a41a4d732",
+    "url": "/SL-Covid/static/js/runtime-main.df089826.js"
+  }
+]);
